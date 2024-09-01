6298b7580345253477bae0c3fe0dbb06eed9bcb2
rifatjahanff <jahanrifat953@gmail.com>
2024-09-01 17:55:28 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,59 @@
+for (let i = 0; i <=1000; i++) {
+    console.log(i);
+    
+}
+
+let x = 5
+let y = 5
+let z = x+y
+let ans = z
+
+console.log(ans+3);
+
+let numbernamota = 5
+for (let i = 1; i <=10; i++) {
+    console.log(numbernamota+" x "+i+"="+numbernamota*i);
+    
+}
+
+let nnumbernamota = 6
+for (let i = 1; i <=10; i++) {
+    console.log(nnumbernamota+" x "+i+"="+nnumbernamota*i);
+    
+}
+
+let nnnumbernamota = 7
+for (let i = 1; i <=10; i++) {
+    console.log(nnnumbernamota+" x "+i+"="+nnnumbernamota*i);
+    
+}
+
+let nnnnumbernamota = 8
+for (let i = 1; i <=10; i++) {
+    console.log(nnnnumbernamota*i);
+    
+}
+
+let number = -3
+
+if (number>=100) {
+    console.log("ato paknami koris ken");
+
+} 
+else if (number<=100 && number>=80){
+    console.log("Tumi A+ payeso");
+}
+else if (number<=80 && number>=33){
+    console.log("Tumi pass korco");
+}
+else if (number<=33 && number>=0){
+    console.log("You are a loser");
+}
+// else if (number<=0 && =-5){
+//     console.log("You are rasticted");
+// }
+
+
+
+
+
